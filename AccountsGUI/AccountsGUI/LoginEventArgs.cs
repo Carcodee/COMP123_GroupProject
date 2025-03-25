cff52db1729ff3412a67f31c4a2fdcb5d8072cf0
CARLOS\carlo <carlosmatecki2@gmail.com>
2025-03-25 15:53:47 -0400

    Person Class

---
+++ AccountsGUI/AccountsGUI/LoginEventArgs.cs
@@ -0,0 +1,29 @@
+﻿namespace COMP123_GroupProject;
+
+
+public enum LoginEventType
+{
+    None,
+    Login,
+    Logout
+}
+public class DayTime
+{
+}
+public class LoginEventArgs : EventArgs
+{
+    public string PersonName { get; }
+    public bool Success { get; }
+    public DayTime Time { get; }
+    public LoginEventType EventType { get; }
+
+    public LoginEventArgs(string username, bool success, LoginEventType loginEventType) : base() 
+    {
+        PersonName = username;
+        Success = success;
+        EventType = loginEventType;
+        //handle type
+        //time = someutilfunc
+    }
+    
+}
---
+++ AccountsGUI/AccountsGUI/Person.cs
@@ -0,0 +1,58 @@
+﻿namespace COMP123_GroupProject;
+
+public enum ExceptionType
+{
+    PLACEHOLDER
+    
+}
+public delegate void LoginEventHandler(object sender, LoginEventArgs e);
+public class AccountException : Exception
+{
+    public ExceptionType Type { get; }
+
+    public AccountException(ExceptionType type)
+        : base(type.ToString())
+    {
+        Type = type;
+    }
+}
+public class Person
+{
+    private string password;
+    private LoginEventHandler onLogin;
+
+    public string Sin { get;}
+    public string Name { get;}
+    public bool IsAuthenticated { get; private set; }
+
+
+    public Person(string name, string sin)
+    {
+        Sin = sin;
+        Name = name;
+    }
+
+    public void Login(string password)
+    {
+        if (password != this.password)
+        {
+            IsAuthenticated = false;
+            onLogin?.Invoke(this, new LoginEventArgs(Name, false, LoginEventType.Login));
+            throw new AccountException(ExceptionType.PLACEHOLDER);
+        }
+
+        IsAuthenticated = true;
+        onLogin?.Invoke(this, new LoginEventArgs(Name, true, LoginEventType.Login));
+    }
+    public void Logout()
+    {
+        IsAuthenticated = false;
+        onLogin?.Invoke(this, new LoginEventArgs(Name, true, LoginEventType.Logout));
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - Authenticated: {IsAuthenticated}";
+    }
+}
+
