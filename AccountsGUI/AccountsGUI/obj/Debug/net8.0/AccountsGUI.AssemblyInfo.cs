20d288e78feac793fe9a9ee2dfd504d196febbaf
KevinKhuu01 <kevinkhuu01@gmail.com>
2025-03-30 19:00:09 -0400

    Updated Logger.cs from Zuck

--- AccountsGUI/AccountsGUI/obj/Debug/net8.0/AccountsGUI.AssemblyInfo.cs
+++ AccountsGUI/AccountsGUI/obj/Debug/net8.0/AccountsGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountsGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee03f8c5bea1ff0d6902d450eb6f345bff733ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7172c95c1ff2c61cd82a3a9fd84e2f222f0b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountsGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountsGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
