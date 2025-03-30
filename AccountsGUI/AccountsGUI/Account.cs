4fdf6e72c8913e25aa8fcc50391779806b4b401c
Zuhriddin <111017966+Zuck7@users.noreply.github.com>
2025-03-30 16:54:23 -0400

    Account commit
    Commited

--- AccountsGUI/AccountsGUI/Account.cs
+++ AccountsGUI/AccountsGUI/Account.cs
@@ -5,63 +5,69 @@ namespace AccountsGUI;
 
 abstract class Account 
 {
-        protected List<Person> users { get; } //?
+        protected List<Person> users { get; }
         public List<Transaction> transactions { get; }
-        private int LAST_NUMBER = 100_000;
-        private event TransactionEventhandler OnTransaction; //?
-
-        protected decimal Balance
-        {
-            public get { } //?
-            protected set {} //?
-        }
-
-        protected decimal LowestBalance
-        {
-            public get { } //?
-            protected set { } //?
-        }
-
-        string Number
-        {
-            public get { } //?
-        }
-
+        private static int LAST_NUMBER = 100_000;
+        private event TransactionEventhandler OnTransaction;
+        public decimal Balance { get; protected set; }
+        public decimal LowestBalance { get; protected set; }
+        public string Number { get; }
         public Account(string type, decimal balance)
         {
-            Number.Concat(); //?
+            Number = $"{type}{LAST_NUMBER}";
             LAST_NUMBER = LAST_NUMBER + 1;
-            Balance = balance; //?
-            LowestBalance = 0; //?
+            Balance = balance;
+            LowestBalance = balance;
         }
-
         protected void Deposit(decimal balance, Person person)
         {
-            Balance += balance;
+            Balance = balance;
             LowestBalance = Balance;
-            object Transaction = Number;
-            transactions.Add(person);
+            Transaction t = new Transaction(this.Number, this.Balance, person);
+            transactions.Add(t);
         }
-
-        public void AddUser(Person Person)
+        public void AddUser(Person user)
         {
+            users.Add(user);
         }
-
-        public bool IsUser(string name)
+        public bool IsUser(Person name)
         {
+            foreach (Person person in users)
+            {
+                if (person.Name == name.Name && person.SIN == name.SIN)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
-
-        public abstract void PrepareMonthlyReport()
-        {
-        }
+        public abstract void PrepareMonthlyReport();
 
         public virtual void OnTransactionOccur(object sender, TransactionEventArgs e)
         {
-            
+            OnTransaction?.invoke(sender, e);
         }
 
         public override string ToString()
         {
+            string result = $"Account Number: {Number}\n";
+
+            result += $"Users\n";
+            foreach (Person user in users)
+            {
+                result += $"{user.Name}\n";
+            }
             
+            result += $"Balance: {Balance}\n";
+            result += $"Transactions\n";
+            if (transactions.Count == 0)
+            {
+                result += "No transactions available\n";
+            } else
+                foreach (Transaction t in transactions)
+                {
+                    result += $"{t}\n";
+                }
+            return result;
         }
 }
