ad1b20dcdcf9b039a448afdcc4e4c265597ebe9f
CARLOS\carlo <carlosmatecki2@gmail.com>
2025-03-31 00:17:39 -0400

    Bank class, I still need to merge so I can finish.

--- AccountsGUI/AccountsGUI/Bank.cs
+++ AccountsGUI/AccountsGUI/Bank.cs
@@ -1,58 +1,139 @@
-﻿namespace COMP123_GroupProject;
+﻿using System.Text.Json;
+
+namespace COMP123_GroupProject;
 public class Account
 {
 }
-public class Bank
+public static class Bank
 {
-    private readonly Dictionary<string, Account> ACCOUNTS;
-    private readonly Dictionary<string, Person> USERS;
+    static private readonly Dictionary<string, Account> ACCOUNTS;
+    static private readonly Dictionary<string, Person> USERS;
 
-    Bank()
+    static Bank()
     {
         
     }
 
-    public void AddUser()
+    public static void AddUser(string name, string sin)
     {
-        
+        try
+        {
+            if (USERS.ContainsKey(sin))
+            {
+                throw new AccountException(ExceptionType.PLACEHOLDER);
+            }
+            else
+            {
+                Person newUser = new Person(name, sin);
+                USERS.Add(sin, newUser);
+            }
+        }
+        catch (AccountException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
-    public void AddAccount(Account account)
+    public static void AddAccount(Account account)
     {
-        
+        try
+        {
+            // if (ACCOUNTS.ContainsKey(account.number))
+            // {
+            //     throw new AccountException(ExceptionType.PLACEHOLDER);
+            // }
+            // else
+            // {
+            //     // ACCOUNTS.Add(account.number, account);
+            // }
+        }
+        catch (AccountException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }   
     }
 
-    public void AddUserToAccount(string number, string name)
+    public static void AddUserToAccount(string number, string name)
     {
-        
+        Person person = GetUser(name);
+        Account account = GetAccount(number);
+        // account.AddUser(person);
     }
     
-    public Account GetAccount(string number)
+    public static Account GetAccount(string number)
     {
+        try
+        {
+            if (!ACCOUNTS.ContainsKey(number))
+            {
+                throw new AccountException(ExceptionType.PLACEHOLDER);
+            } 
+            return ACCOUNTS.GetValueOrDefault(number);
+        }
+        catch (AccountException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
+    public static Person GetUser(string name)
+    {
+        try
+        {
+            foreach (var user in USERS)
+            {
+                if (user.Value.Name == name)
+                {
+                    return user.Value;
+                } 
+            }
 
-        return new Account();
+            throw new AccountException(ExceptionType.PLACEHOLDER);
+        }
+        catch (AccountException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
     
-    public Person GetUser(string number)
+    public static void PrintAccounts()
     {
-        return new Person("","");
+        foreach (var account in ACCOUNTS)
+        {
+            Console.WriteLine(account.ToString());
+        }
     }
     
-    public void PrintAccounts()
+    public static void PrintUsers()
     {
-        
-    }
-    
-    public void PrintUsers()
-    {
-        
+        foreach (var user in USERS)
+        {
+            Console.WriteLine(user.ToString());
+        }
     }
 
-    public void SaveAccounts(string filename)
+    public static void SaveAccounts(string filename)
     {
-        
+        StreamWriter writer= new StreamWriter(filename);
+        foreach (var account in ACCOUNTS)
+        {
+            string jsonText = JsonSerializer.Serialize(account);
+            writer.Write(jsonText);
+        }
+        writer.Close();
     }
-    public void SaveUsers(string filename)
+    public static void SaveUsers(string filename)
     {
-        
+        StreamWriter writer= new StreamWriter(filename);
+        foreach (var user in USERS)
+        {
+            string jsonText = JsonSerializer.Serialize(user);
+            writer.Write(jsonText);
+        }
+        writer.Close();
     }
+
 }
