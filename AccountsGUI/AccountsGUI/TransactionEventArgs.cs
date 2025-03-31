2a85faabccfa565a96aaf9f191fe9434980ff242
micheal-code10 <123703875+micheal-code10@users.noreply.github.com>
2025-03-31 19:19:57 -0400

    push number one

---
+++ AccountsGUI/AccountsGUI/TransactionEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankingApplication
+{
+    public class TransactionEventArgs : LoginEventArgs
+    {
+        public decimal Amount { get; }
+
+        public TransactionEventArgs(string personName, decimal amount, bool success)
+            : base(personName, success, LoginEventType.None)
+        {
+            Amount = amount;
+        }
+    }
+}
